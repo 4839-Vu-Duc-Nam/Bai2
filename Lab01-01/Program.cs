e9e09d79a300c45bf5a492a4137de0556427a8d4
4839-Vu-Duc-Nam <yolodinam.ok@gmail.com>
2025-09-13 19:17:21 +0700

    Add files via upload

---
+++ Lab01-01/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab01_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("\n=== MENU ===");
+                Console.WriteLine("1. Them sinh vien");
+                Console.WriteLine("2. Xuat danh sach sinh vien");
+                Console.WriteLine("0. Thoat");
+                Console.Write("Chon chuc nang: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Student s = new Student();
+                        s.Input();
+                        students.Add(s);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("\n=== Danh sach sinh vien ===");
+                        foreach (var st in students)
+                        {
+                            st.Show();
+                        }
+                        break;
+
+                    case "0":
+                        exit = true;
+                        Console.WriteLine("Ket thuc chuong trinh.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Lua chon khong hop le. Thu lai!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Lab01-01/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab01_01
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+        public double AverageScore { get; set; }
+
+        // Ham nhap thong tin sinh vien
+        public void Input()
+        {
+            Console.Write("Nhap ma so: ");
+            Id = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap ho ten: ");
+            Name = Console.ReadLine();
+
+            Console.Write("Nhap khoa: ");
+            Faculty = Console.ReadLine();
+
+            Console.Write("Nhap diem trung binh: ");
+            AverageScore = double.Parse(Console.ReadLine());
+        }
+
+        // Ham hien thi thong tin sinh vien
+        public void Show()
+        {
+            Console.WriteLine("Ma so: {0}, Ho ten: {1}, Khoa: {2}, Diem TB: {3}",
+                Id, Name, Faculty, AverageScore);
+        }
+    }
+}
